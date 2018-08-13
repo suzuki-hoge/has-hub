60227aaf1fe5a39cdcf8b475c4b74b334a0c219d
suzuki-hoge <user.ryo@gmail.com>
2018-08-13 22:35:29 +0900

    replace epic validations. fixed #35.

--- src/HasHub/Object/Object/Validator.hs
+++ src/HasHub/Object/Object/Validator.hs
@@ -1,6 +1,10 @@
 module HasHub.Object.Object.Validator
 (
   areAllIn
+, noDuplication
+, linkNumberFormat
+, parentNumberFormat
+, linking
 , module HasHub.FixMe
 )
 where
@@ -8,57 +12,13 @@ where
 
 import Text.Regex.Posix ((=~))
 
+import Data.List (nub, sort, (\\))
+import Data.Maybe (mapMaybe, listToMaybe)
+
 import HasHub.Object.Object.Type
 
 import HasHub.FixMe (Error, Validation(..))
 import qualified HasHub.FixMe as F (areAllIn)
-
-
---type RowNum = Int
---type Definition = (RowNum, EpicLinkNumber)
---type Parent = (RowNum, ParentEpicNumber)
---
---
---validateLinking :: [Definition] -> [Parent] -> Validation [String] [()]
---validateLinking definitions parents = mergeAll (map validate parents) []
---  where
---    filterBy :: [Definition] -> Parent -> Maybe Definition
---    filterBy definitions parent = listToMaybe $ filter (cond parent) definitions
---      where
---        cond :: Parent -> Definition -> Bool
---        cond (_, QuestionEpicNumber(qen)) (_, EpicLinkNumber(eln)) = qen == eln
---        cond _ _ = False
---
---    validate :: Parent -> Validation [String] [()]
---    validate (pn, eln) = case filterBy definitions (pn, eln) of
---      Just (dn, _) -> if dn < pn then Success [()] else Failure ["invalid order"]
---      Nothing -> Failure ["no link"]
---
---
---epicLinkNumberFormat :: [EpicLinkNumber] -> Validation [Error] ()
---epicLinkNumberFormat elns = mergeAll (map validate elns) []
---  where
---    validate :: EpicLinkNumber -> Validation [Error] ()
---    validate (EpicLinkNumber s) = if s `isNumberedBy` '?' then Success () else Failure ["format error"] -- todo (FormatError, ?1)
---
---
---
---parentEpicNumberFormat :: [ParentEpicNumber] -> Validation [Error] [()]
---parentEpicNumberFormat pens = mergeAll (map validate pens) []
---  where
---    validate :: ParentEpicNumber -> Validation [Error] ()
---    validate (SharpEpicNumber s) = if s `isNumberedBy` '#' then Success () else Failure ["format error"] -- todo (FormatError, ?1)
---    validate (QuestionEpicNumber s) = if s `isNumberedBy` '?' then Success () else Failure ["format error"] -- todo (FormatError, ?1)
---
---
---isNumberedBy :: String -> Char -> Bool
---isNumberedBy s c = s =~ ("^\\" ++ [c] ++ "[0-9]+$")
---
---
---validateNoDuplicate :: [EpicLinkNumber] -> Validation [Error] ()
---validateNoDuplicate xs = if dups == [] then Success () else Failure ["dup"]
---  where
---    dups = sort xs \\ (nub . sort) xs
 
 
 areAllIn :: [ParentEpicNumber] -> [Epic] -> Validation [Error] ()
@@ -67,3 +27,66 @@ areAllIn needles haystacks = concatMap toEpicNumberIfSharp needles `F.areAllIn` 
     toEpicNumberIfSharp :: ParentEpicNumber -> [EpicNumber]
     toEpicNumberIfSharp (SharpEpicNumber s) = [EpicNumber $ (read . tail) s]
     toEpicNumberIfSharp (QuestionEpicNumber _) = []
+
+
+noDuplication :: [EpicLinkNumber] -> Validation [Error] ()
+noDuplication numbers = case dups of
+  [] -> Success ()
+  xs -> Failure $ map (\(EpicLinkNumber s) -> "duplicate error: " ++ s) xs
+  where
+    dups = sort numbers \\ (nub . sort) numbers
+
+
+linkNumberFormat :: [EpicLinkNumber] -> Validation [Error] () -- todo (FormatError, ?1)
+linkNumberFormat numbers = case mapMaybe validate numbers of
+  [] -> Success () -- todo delegate to FixMe
+  xs -> Failure xs
+  where
+    validate :: EpicLinkNumber -> Maybe Error
+    validate (EpicLinkNumber s) = s `isNumberedBy` '?'
+
+
+parentNumberFormat :: [ParentEpicNumber] -> Validation [Error] () -- todo (FormatError, ?1)
+parentNumberFormat numbers = case mapMaybe validate numbers of
+  [] -> Success () -- todo delegate to FixMe
+  xs -> Failure xs
+  where
+    validate :: ParentEpicNumber -> Maybe Error
+    validate (SharpEpicNumber s)    = s `isNumberedBy` '#'
+    validate (QuestionEpicNumber s) = s `isNumberedBy` '?'
+
+
+isNumberedBy :: String -> Char -> Maybe Error
+isNumberedBy s c = if s =~ ("^\\" ++ [c] ++ "[0-9]+$")
+  then Nothing
+  else Just $ "format error: " ++ s
+
+
+type LineNum = Int
+type Definition = (LineNum, EpicLinkNumber)
+type Parent = (LineNum, ParentEpicNumber)
+
+linking :: [Definition] -> [Parent] -> Validation [Error] ()
+linking definitions parents = case mapMaybe (validate definitions) parents of
+  [] -> Success () -- todo delegate to FixMe
+  xs -> Failure xs
+  where
+    validate :: [Definition] -> Parent -> Maybe Error
+    validate definitions (_, (SharpEpicNumber _)) = Nothing
+    validate definitions parent = case definitions `filterBy` parent of
+      Just (dn, _) -> if dn < _n parent then Nothing else Just $ "use " ++ _s parent ++ " on line " ++ (show . _n) parent ++ ", but " ++ _s parent ++ " is defined at line " ++ show dn
+      Nothing -> Just $ "use " ++ _s parent ++ " on line " ++ (show . _n) parent ++ ", but " ++ _s parent ++ " is not defined"
+      where
+        filterBy :: [Definition] -> Parent -> Maybe Definition
+        filterBy definitions parent = listToMaybe $ filter (cond parent) definitions
+          where
+            cond :: Parent -> Definition -> Bool
+            cond (_, QuestionEpicNumber(n1)) (_, EpicLinkNumber(n2)) = n1 == n2
+        _n :: Parent -> LineNum
+        _n (n, _) = n
+
+        _s :: Parent -> String
+        _s (_, QuestionEpicNumber(s)) = s
+
+
+-- todo instance FixMe ???
--- test/HasHub/Object/Object/ValidatorSpec.hs
+++ test/HasHub/Object/Object/ValidatorSpec.hs
@@ -4,6 +4,7 @@ module HasHub.Object.Object.ValidatorSpec where
 import Test.Hspec
 
 import HasHub.Object.Object.Validator
+import HasHub.Object.Object.Type
 
 import qualified Fixture as F
 
@@ -27,3 +28,133 @@ spec = do
         let act = [F.sharpEpicNumber] `areAllIn` []
 
         act `shouldBe` Failure [show F.epicNumber1]
+
+  describe "no duplication" $ do
+    describe "success" $ do
+      it "double" $ do
+        let act = noDuplication [EpicLinkNumber "?1", EpicLinkNumber "?2"]
+
+        act `shouldBe` Success ()
+
+      it "single" $ do
+        let act = noDuplication [EpicLinkNumber "?1"]
+
+        act `shouldBe` Success ()
+
+      it "empty" $ do
+        let act = noDuplication []
+
+        act `shouldBe` Success ()
+
+    describe "failure" $ do
+      it "double" $ do
+        let act = noDuplication [EpicLinkNumber "?2", EpicLinkNumber "?1", EpicLinkNumber "?3", EpicLinkNumber "?1", EpicLinkNumber "?2"]
+
+        act `shouldBe` Failure [
+            "duplicate error: ?1"
+          , "duplicate error: ?2"
+          ]
+
+  describe "link-number format" $ do
+    describe "success" $ do
+      it "starts with ? and number only" $ do
+        let act = linkNumberFormat [EpicLinkNumber "?1", EpicLinkNumber "?123"]
+
+        act `shouldBe` Success ()
+
+      it "empty" $ do
+        let act = linkNumberFormat []
+
+        act `shouldBe` Success ()
+
+    describe "failure" $ do
+      it "valid, no-mark, sharp-mark, no-number, contain-non-number, double-mark" $ do
+        let act = linkNumberFormat [EpicLinkNumber "?1", EpicLinkNumber "1", EpicLinkNumber "#1", EpicLinkNumber "?", EpicLinkNumber "? 1", EpicLinkNumber "??1"]
+
+        act `shouldBe` Failure [
+            "format error: 1"
+          , "format error: #1"
+          , "format error: ?"
+          , "format error: ? 1"
+          , "format error: ??1"
+          ]
+
+  describe "parent-number format" $ do
+    describe "success" $ do
+      it "starts with ? and number only, starts with # and number only" $ do
+        let act = parentNumberFormat [QuestionEpicNumber "?1", SharpEpicNumber "#1", SharpEpicNumber "#123"]
+
+        act `shouldBe` Success ()
+
+      it "empty" $ do
+        let act = parentNumberFormat []
+
+        act `shouldBe` Success ()
+
+    describe "failure" $ do
+      it "valid, sharp-only, sharp-and-contain-non-number, no-mark, double-mark" $ do
+        let act = parentNumberFormat [SharpEpicNumber "#1", SharpEpicNumber "#", SharpEpicNumber "# 1", QuestionEpicNumber "1", QuestionEpicNumber "?#"]
+
+        act `shouldBe` Failure [
+            "format error: #"
+          , "format error: # 1"
+          , "format error: 1"
+          , "format error: ?#"
+          ]
+
+  describe "linking" $ do
+    describe "success" $ do
+      it "def at line-1, use at line-2" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] [(2, QuestionEpicNumber "?1")]
+
+        act `shouldBe` Success ()
+
+      it "def at line-1, no use" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] []
+
+        act `shouldBe` Success ()
+
+      it "def at line-1, sharp on line-1" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] [(1, SharpEpicNumber "#1")]
+
+        act `shouldBe` Success ()
+
+      it "def at line-1 and line-2, use at line-2 and line-4 and sharp on line-3" $ do
+        let act = linking [(1, EpicLinkNumber "?1"), (2, EpicLinkNumber "?2")] [(2, QuestionEpicNumber "?1"), (3, SharpEpicNumber "#1"), (4, QuestionEpicNumber "?2")]
+
+        act `shouldBe` Success ()
+
+      it "empty" $ do
+        let act = linking [] []
+
+        act `shouldBe` Success ()
+
+    describe "failure" $ do
+      it "no def, use at line-1" $ do
+        let act = linking [] [(1, QuestionEpicNumber "?1")]
+
+        act `shouldBe` Failure ["use ?1 on line 1, but ?1 is not defined"]
+
+      it "def at line-1, use at line-2 but mismatch number" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] [(2, QuestionEpicNumber "?2")]
+
+        act `shouldBe` Failure ["use ?2 on line 2, but ?2 is not defined"]
+
+      it "def at line-1, use at line-1" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] [(1, QuestionEpicNumber "?1")]
+
+        act `shouldBe` Failure ["use ?1 on line 1, but ?1 is defined at line 1"]
+
+      it "def at line-2, use at line-1" $ do
+        let act = linking [(2, EpicLinkNumber "?1")] [(1, QuestionEpicNumber "?1")]
+
+        act `shouldBe` Failure ["use ?1 on line 1, but ?1 is defined at line 2"]
+
+      it "def at line-1, use at line-1 and mismatch number and sharp on line-2 and mismatch number" $ do
+        let act = linking [(1, EpicLinkNumber "?1")] [(1, QuestionEpicNumber "?1"), (2, SharpEpicNumber "#1"), (2, QuestionEpicNumber "?2"), (3, QuestionEpicNumber "?2")]
+
+        act `shouldBe` Failure [
+            "use ?1 on line 1, but ?1 is defined at line 1"
+          , "use ?2 on line 2, but ?2 is not defined"
+          , "use ?2 on line 3, but ?2 is not defined"
+          ]
