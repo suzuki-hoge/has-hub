7e9013162abbecb034a758ed4d28fe867f3a9f23
suzuki-hoge <user.ryo@gmail.com>
2019-10-09 16:02:41 +0900

    tmp: hard code.

--- src/HubBoard/Object/Pipeline/Mapper.hs
+++ src/HubBoard/Object/Pipeline/Mapper.hs
@@ -10,8 +10,10 @@ import           HubBoard.Fetcher
 instance FromJSON Pipeline where
     parseJSON (Object v) = Pipeline <$> (v .: "id") <*> (v .: "name")
 
+--refer :: IO [Pipeline]
+--refer = getFromZenHub toResource parse
+--  where
+--    toResource = printf "%s/board"
+--    parse = fromJust . (decode >=> parseMaybe (.: "pipelines"))
 refer :: IO [Pipeline]
-refer = getFromZenHub toResource parse
-  where
-    toResource = printf "%s/board"
-    parse = fromJust . (decode >=> parseMaybe (.: "pipelines"))
+refer = return [Pipeline "5bd92a1cd643eb36430c27c6" "超先のバックログ", Pipeline "5c3401ab32f2e5319a433524" "バックログ", Pipeline "5be11beb4590dc110b6dcf91" "カレンダーの予定", Pipeline "5bd92a1cd643eb36430c27c9" "スプリントバックログ", Pipeline "5c9c29509a9dfd529273578c" "今日中", Pipeline "5bd92a1cd643eb36430c27ca" "進行中", Pipeline "5bdfe50a4590dc110b6dc19c" "レビュー待ち"]
